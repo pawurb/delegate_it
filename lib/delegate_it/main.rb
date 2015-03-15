9e7c8c58daeeeb4907ab75fff2671ea31eb82ec6
pawurb <p.urbanek89@gmail.com>
2015-03-15 17:24:24 +0200

    multiple delegations and allow nil option works

--- lib/delegate_it/main.rb
+++ lib/delegate_it/main.rb
@@ -1,12 +1,37 @@
 #encoding: UTF-8
 module DelegateIt
-  def delegate *methods, receiver
-    define_method :method_missing do |method, *args, &block|
-      if methods.include?(method)
-        self.send(receiver[:to]).send(method, *args, &block)
-      else
-        raise NoMethodError
+  def self.included(klass)
+    klass.instance_eval do
+      def delegate *methods, options
+        @@delegated_settings ||= []
+        methods.each do |method|
+          @@delegated_settings.push(Struct.new(:name, :options).new(method, options))
+        end
       end
+    end
+  end
+
+  def method_missing(method, *)
+    setting = @@delegated_settings.select do |s|
+      s.name == method
+    end.first
+
+    if setting
+      receiver = if self.respond_to?(setting.options[:to])
+        self.send(setting.options[:to])
+      else
+        nil
+      end
+
+      if receiver
+        receiver.send(method)
+      elsif setting.options[:allow_nil]
+        nil
+      else
+        receiver.send(method)
+      end
+    else
+      super
     end
   end
 end
--- spec/delegate_it/main_spec.rb
+++ spec/delegate_it/main_spec.rb
@@ -2,11 +2,17 @@
 require 'spec_helper'
 
 class Dummy
-  extend DelegateIt
-  delegate :name, to: :has_name
+  include DelegateIt
+  delegate :name, to: :name_delegate
+  delegate :hello, to: :no_exist, allow_nil: true
+  delegate :there, to: :there_delegate, allow_nil: true
 
-  def has_name
-    Struct.new(:name).new('name')
+  def name_delegate
+    Struct.new(:name).new('name_val')
+  end
+
+  def there_delegate
+    Struct.new(:there).new('there_val')
   end
 end
 
@@ -17,7 +23,43 @@ describe 'DelegateIt' do
 
   describe "standard delegation" do
     it "works" do
-      expect(subject.name).to eq 'name'
+      expect(subject.name).to eq 'name_val'
+    end
+  end
+
+  describe "allow_nil case" do
+    context "receiver is nil" do
+      it "returns nil" do
+        expect(subject.hello).to eq nil
+      end
+    end
+
+    context "receiver exists" do
+      it "delegates like usual" do
+        expect(subject.there).to eq 'there_val'
+      end
+    end
+  end
+
+  describe "specifying prefix" do
+    context "prefixing by delegate name" do
+      it "executes the correctc method on delegate" do
+
+      end
+    end
+
+    context "custom prefix value" do
+      it "executes the correctc method on delegate" do
+
+      end
+    end
+  end
+
+  describe "method does not exist" do
+    it "raises no_method error" do
+      expect do
+        subject.not_there
+      end.to raise_error(NoMethodError)
     end
   end
 end
